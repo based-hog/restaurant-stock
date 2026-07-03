c5dd581f95c1ad8c255018ec137479bc7e5aa30d
based-hog <chizhik.dania@gmail.com>
2026-07-04 00:48:34 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,26 @@
 import "./globals.css";
+import { useEffect } from "react";
+
 export const metadata = {
-  title: "Restaurant Stock",
-  description: "Учет прихода и списания"
+  title: "Хинкальня Склад",
+  description: "Учет прихода и списания",
 };
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js");
+    }
+  }, []);
+
   return (
     <html lang="ru">
-      <link rel="manifest" href="/manifest.json" />
-<meta name="theme-color" content="#2ecc71" />
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#2ecc71" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
+
       <body>{children}</body>
     </html>
   );
